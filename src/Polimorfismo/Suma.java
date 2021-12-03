749a6b3e0f2372e9f4be21c89486c7e0926685b9
Francisco Garache <franciscogarach3@gmail.com>
2021-12-03 17:23:15 -0600

    polimnorfismo

--- src/Polimorfismo/Suma.java
+++ src/Polimorfismo/Suma.java
@@ -2,14 +2,13 @@ package Polimorfismo;
 
 public class Suma extends operaciones{
     @Override
-    public void operaciones() {
-
+    public void operaciones(){
+        resultado = valor1+valor2;
     }
 
     @Override
-    public void operacciones(){
-            resultado = valor1+valor2;
+    public void operacciones() {
 
-        }
+    }
 
 }
